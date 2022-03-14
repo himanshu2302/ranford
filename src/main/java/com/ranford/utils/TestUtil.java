30709387b9a84f0afef683efc3f39b527a7b854e
Shubhanshu Dubey <anshu.ishani@love.com>
2022-03-15 00:20:16 +0530

    test data method

--- src/main/java/com/ranford/utils/TestUtil.java
+++ src/main/java/com/ranford/utils/TestUtil.java
@@ -11,49 +11,49 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 
 import com.ranford.base.TestBase;
 
-public class TestUtil extends TestBase{
+public class TestUtil extends TestBase {
 
-	// Defining static wait timings
-	public static long pageLoadTime=40;
-	public static long implicitTime=20;
-	
-	static Workbook book;
-	static Sheet sheet;
-	
-	// Method for switching to the frame
-	public static void switchToFrame() {
-		driver.switchTo().frame("mainpanel");
-	}
-	
-	
-	// Method for getting data from excel sheet
-	public static Object[][] getTestData(String sheetName) throws InvalidFormatException {
-		FileInputStream file = null;
-		
-		try {
-			file = new FileInputStream("C:\\Users\\Himanshu.Dubey\\eclipse-workspace\\Ranford1\\src\\main\\java\\com\\ranford\\testdata\\testdata.xlsx");
-		}
-		
-		catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		try {
-			book = WorkbookFactory.create(file);
-		}
-		
-		catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		sheet = book.getSheet(sheetName);
-		Object [][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
-		for(int i=0; i<sheet.getLastRowNum();i++) {
-			for(int j=0;j<sheet.getRow(0).getLastCellNum();j++) {
-				data[i][j]=sheet.getRow(i+1).getCell(j).toString();
-				
-			}
-		}
-		return data;
-	}
+    // Defining static wait timings
+    public static long pageLoadTime = 40;
+    public static long implicitTime = 20;
+
+    static Workbook book;
+    static Sheet sheet;
+
+    // Method for switching to the frame
+    public static void switchToFrame() {
+        driver.switchTo().frame("mainpanel");
+    }
+
+
+    // Method for getting data from excel sheet
+    public static Object[][] getTestData(String sheetName) throws InvalidFormatException {
+        FileInputStream file = null;
+
+        try {
+            file = new FileInputStream("C:\\Users\\Himanshu.Dubey\\eclipse-workspace\\Ranford1\\src\\main\\java\\com\\ranford\\testdata\\testdata.xlsx");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            book = WorkbookFactory.create(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        sheet = book.getSheet(sheetName);
+        Object[][] data = new Object[sheet.getLastRowNum()][sheet.getRow(0).getLastCellNum()];
+        for (int i = 0; i < sheet.getLastRowNum(); i++) {
+            for (int j = 0; j < sheet.getRow(0).getLastCellNum(); j++) {
+                data[i][j] = sheet.getRow(i + 1).getCell(j).toString();
+
+            }
+        }
+        return data;
+    }
+
+    public void testData() {
+
+    }
 }
